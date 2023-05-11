a11e7ed3a704a4de4291c68c50bf2ac5ed9ed2fc
Paula Gawron <276242@student.pwr.edu.pl>
2023-05-11 22:06:36 +0200

    Added ICalculator interface

---
+++ Homework/src/ICalculator.java
@@ -0,0 +1,8 @@
+public interface ICalculator {
+    double add (double a, double b);
+    double subtract (double a, double b);
+    double multiply (double a, double b);
+    double divide (double a, double b);
+    double percentage (double a, double b);
+    double factorial (int n);
+}
